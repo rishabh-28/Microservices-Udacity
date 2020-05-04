8da5dd9e3aff1310ab6887d225f1aeb38e44deb1
EC2 Default User <ec2-user@ip-172-31-24-111.us-west-2.compute.internal>
2020-05-04 19:49:08 +0000

    Docker_out complete

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ WORKDIR /app
 ## Step 2:
 # Copy source code to working directory
 COPY . app.py /app/
-
+RUN pip install --upgrade pip && pip install --trusted-host pypi.python.org -r requirements.txt
 
 ## Step 4:
 # Expose port 80
--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t udacity_devops .
+docker build -t rrishabhbansal96/udacity_devops .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -it --name udacity_devops -p 8000:80 udacity_devops
+docker run -it --name udacity_devops -p 8000:80 rrishabhbansal96/udacity_devops
